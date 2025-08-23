82a62ec3596d1d924a0596e7d0ed0651665b1e17
Hrafn Orri Hrafnkelsson <hrafn@vidfjord.is>
2025-08-23 10:02:37 +0000

    update

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,9 +1,6 @@
 use bevy::{prelude::*, window::PresentMode};
 use bevy_state_ui::prelude::*;
 
-#[derive(Component)]
-struct RootNode;
-
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins
@@ -18,7 +15,7 @@ fn main() {
             }),))
         .add_systems(Startup, setup)
         .add_systems(Update, update_button_interactions)
-        .add_systems(Update, render.run_if(ui_state_changed::<State>))
+        .add_systems(Update, ui_state_render::<State>)
         .register_ui_state::<State>()
         .run();
 }
@@ -33,57 +30,57 @@ pub struct State {
     pub hovered: bool,
 }
 
-fn render(mut commands: Commands, state: Res<State>, q_root: Query<Entity, With<RootNode>>) {
-    info!("render");
+impl StateRender for State {
+    fn render(&self, mut commands: EntityCommands) {
+        info!("render");
 
-    for entity in q_root.iter() {
-        commands.entity(entity).despawn_recursive();
-    }
-
-    commands
-        .spawn((
-            RootNode,
-            Node {
+        commands
+            .insert(Node {
                 width: Val::Percent(100.0),
                 height: Val::Percent(100.0),
                 ..default()
-            },
-        ))
-        .with_children(|parent| {
-            parent
-                .spawn((
-                    Node {
-                        width: Val::Percent(40.0),
-                        height: Val::Percent(15.0),
-                        top: Val::Percent(42.5),
-                        left: Val::Percent(30.0),
-                        justify_content: JustifyContent::Center,
-                        position_type: PositionType::Absolute,
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    BackgroundColor(if state.hovered {
-                        Color::srgb(1.0, 1.0, 1.0).into()
-                    } else {
-                        Color::srgb(0.0, 0.0, 0.0).into()
-                    }),
-                    Button { ..default() },
-                ))
-                .with_children(|parent| {
-                    parent.spawn((
-                        Text::new("I am a button"),
-                        TextColor(if !state.hovered {
+            })
+            .with_children(|parent| {
+                parent
+                    .spawn((
+                        Node {
+                            width: Val::Percent(40.0),
+                            height: Val::Percent(15.0),
+                            top: Val::Percent(42.5),
+                            left: Val::Percent(30.0),
+                            justify_content: JustifyContent::Center,
+                            position_type: PositionType::Absolute,
+                            align_items: AlignItems::Center,
+                            ..default()
+                        },
+                        BackgroundColor(if self.hovered {
                             Color::srgb(1.0, 1.0, 1.0).into()
                         } else {
                             Color::srgb(0.0, 0.0, 0.0).into()
                         }),
-                        TextFont {
-                            font_size: 40.0,
-                            ..default()
+                        Button { ..default() },
+                        if self.hovered {
+                            Interaction::Hovered
+                        } else {
+                            Interaction::None
                         },
-                    ));
-                });
-        });
+                    ))
+                    .with_children(|parent| {
+                        parent.spawn((
+                            Text::new("I am a button"),
+                            TextColor(if !self.hovered {
+                                Color::srgb(1.0, 1.0, 1.0).into()
+                            } else {
+                                Color::srgb(0.0, 0.0, 0.0).into()
+                            }),
+                            TextFont {
+                                font_size: 40.0,
+                                ..default()
+                            },
+                        ));
+                    });
+            });
+    }
 }
 
 fn update_button_interactions(
@@ -95,11 +92,11 @@ fn update_button_interactions(
     }
     for interaction in &q_interaction {
         match interaction {
-            Interaction::None | Interaction::Pressed => {
-                state.hovered = false;
-            }
-            Interaction::Hovered => {
+            Interaction::Pressed | Interaction::Hovered => {
                 state.hovered = true;
+            }
+            Interaction::None => {
+                state.hovered = false;
             }
         }
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,84 @@
-pub mod app_ext;
+use std::{
+    hash::{BuildHasher, Hash, Hasher},
+    marker::PhantomData,
+    u64,
+};
+
+use bevy::{diagnostic::FrameCount, platform::hash::FixedState, prelude::*};
 
 pub mod prelude {
-    pub use crate::app_ext::{ui_state_changed, StateUiAppExt};
+    pub use crate::{ui_state_render, StateRender, StateUiAppExt};
+}
+
+#[derive(Component)]
+pub struct RootNode<T> {
+    phantom: PhantomData<T>,
+}
+
+pub trait StateRender {
+    fn render(&self, commands: EntityCommands);
+}
+
+#[derive(Resource)]
+pub struct RenderedHash<T> {
+    phantom: PhantomData<T>,
+    last_frame_check: u32,
+    last_hash_value: u64,
+}
+
+pub trait StateUiAppExt {
+    fn register_ui_state<TState>(&mut self) -> &mut Self
+    where
+        TState: Resource;
+}
+
+impl StateUiAppExt for App {
+    fn register_ui_state<TState>(&mut self) -> &mut Self
+    where
+        TState: Resource,
+    {
+        self.insert_resource(RenderedHash::<TState> {
+            phantom: PhantomData::default(),
+            last_frame_check: 0,
+            last_hash_value: 0,
+        });
+        self
+    }
+}
+
+pub fn ui_state_render<TState>(
+    mut commands: Commands,
+    frame_count: Res<FrameCount>,
+    mut hash: ResMut<RenderedHash<TState>>,
+    state: Option<Res<TState>>,
+    q_root: Query<Entity, With<RootNode<TState>>>,
+) where
+    TState: Resource + Hash + StateRender,
+{
+    let Some(state) = state else { return };
+
+    if hash.last_frame_check == frame_count.0 {
+        return;
+    }
+
+    let mut hasher = FixedState::default().build_hasher();
+    state.hash(&mut hasher);
+    let value = hasher.finish();
+
+    if hash.last_hash_value == value {
+        return;
+    }
+
+    hash.last_frame_check = frame_count.0;
+    hash.last_hash_value = value;
+
+    for entity in q_root.iter() {
+        commands.entity(entity).despawn();
+    }
+
+    let commands = commands.spawn(RootNode::<TState> {
+        phantom: PhantomData::default(),
+    });
+
+    state.render(commands);
 }
